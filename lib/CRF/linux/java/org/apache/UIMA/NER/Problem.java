aa19558ece2e3a2922fc6675b09e56a24aa24ab3
zacharyhit <wangjingqihit@gmail.com>
2013-12-02 11:14:20 -0600

    newest lib of Medex

---
+++ lib/CRF/linux/java/org/apache/UIMA/NER/Problem.java
@@ -0,0 +1,119 @@
+
+
+/* First created by JCasGen Thu Jun 06 15:41:26 CDT 2013 */
+package org.apache.UIMA.NER;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Fri Jun 07 16:25:07 CDT 2013
+ * XML source: C:/medex/MedEx_UIMA/desc/NERAnnotatorDescriptor.xml
+ * @generated */
+public class Problem extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Problem.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated  */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Problem() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public Problem(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public Problem(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public Problem(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: Start
+
+  /** getter for Start - gets 
+   * @generated */
+  public int getStart() {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_Start == null)
+      jcasType.jcas.throwFeatMissing("Start", "org.apache.UIMA.NER.Problem");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((Problem_Type)jcasType).casFeatCode_Start);}
+    
+  /** setter for Start - sets  
+   * @generated */
+  public void setStart(int v) {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_Start == null)
+      jcasType.jcas.throwFeatMissing("Start", "org.apache.UIMA.NER.Problem");
+    jcasType.ll_cas.ll_setIntValue(addr, ((Problem_Type)jcasType).casFeatCode_Start, v);}    
+   
+    
+  //*--------------*
+  //* Feature: End
+
+  /** getter for End - gets 
+   * @generated */
+  public int getEnd() {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_End == null)
+      jcasType.jcas.throwFeatMissing("End", "org.apache.UIMA.NER.Problem");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((Problem_Type)jcasType).casFeatCode_End);}
+    
+  /** setter for End - sets  
+   * @generated */
+  public void setEnd(int v) {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_End == null)
+      jcasType.jcas.throwFeatMissing("End", "org.apache.UIMA.NER.Problem");
+    jcasType.ll_cas.ll_setIntValue(addr, ((Problem_Type)jcasType).casFeatCode_End, v);}    
+   
+    
+  //*--------------*
+  //* Feature: Text
+
+  /** getter for Text - gets 
+   * @generated */
+  public String getText() {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_Text == null)
+      jcasType.jcas.throwFeatMissing("Text", "org.apache.UIMA.NER.Problem");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Problem_Type)jcasType).casFeatCode_Text);}
+    
+  /** setter for Text - sets  
+   * @generated */
+  public void setText(String v) {
+    if (Problem_Type.featOkTst && ((Problem_Type)jcasType).casFeat_Text == null)
+      jcasType.jcas.throwFeatMissing("Text", "org.apache.UIMA.NER.Problem");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Problem_Type)jcasType).casFeatCode_Text, v);}    
+  }
+
+    
---
+++ lib/CRF/linux/java/org/apache/UIMA/NLPTools/NER/Problem_Type.java
@@ -0,0 +1,128 @@
+
+/* First created by JCasGen Thu Jun 06 15:20:49 CDT 2013 */
+package org.apache.UIMA.NLPTools.NER;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Mon Jun 10 10:21:25 CDT 2013
+ * @generated */
+public class Problem_Type extends Annotation_Type {
+  /** @generated */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (Problem_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = Problem_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new Problem(addr, Problem_Type.this);
+  			   Problem_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new Problem(addr, Problem_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = Problem.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.apache.UIMA.NLPTools.NER.Problem");
+
+
+
+  /** @generated */
+  final Feature casFeat_Start;
+  /** @generated */
+  final int     casFeatCode_Start;
+  /** @generated */ 
+  public int getStart(int addr) {
+        if (featOkTst && casFeat_Start == null)
+      jcas.throwFeatMissing("Start", "org.apache.UIMA.NLPTools.NER.Problem");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_Start);
+  }
+  /** @generated */    
+  public void setStart(int addr, int v) {
+        if (featOkTst && casFeat_Start == null)
+      jcas.throwFeatMissing("Start", "org.apache.UIMA.NLPTools.NER.Problem");
+    ll_cas.ll_setIntValue(addr, casFeatCode_Start, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_End;
+  /** @generated */
+  final int     casFeatCode_End;
+  /** @generated */ 
+  public int getEnd(int addr) {
+        if (featOkTst && casFeat_End == null)
+      jcas.throwFeatMissing("End", "org.apache.UIMA.NLPTools.NER.Problem");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_End);
+  }
+  /** @generated */    
+  public void setEnd(int addr, int v) {
+        if (featOkTst && casFeat_End == null)
+      jcas.throwFeatMissing("End", "org.apache.UIMA.NLPTools.NER.Problem");
+    ll_cas.ll_setIntValue(addr, casFeatCode_End, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_Text;
+  /** @generated */
+  final int     casFeatCode_Text;
+  /** @generated */ 
+  public String getText(int addr) {
+        if (featOkTst && casFeat_Text == null)
+      jcas.throwFeatMissing("Text", "org.apache.UIMA.NLPTools.NER.Problem");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_Text);
+  }
+  /** @generated */    
+  public void setText(int addr, String v) {
+        if (featOkTst && casFeat_Text == null)
+      jcas.throwFeatMissing("Text", "org.apache.UIMA.NLPTools.NER.Problem");
+    ll_cas.ll_setStringValue(addr, casFeatCode_Text, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	* @generated */
+  public Problem_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_Start = jcas.getRequiredFeatureDE(casType, "Start", "uima.cas.Integer", featOkTst);
+    casFeatCode_Start  = (null == casFeat_Start) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_Start).getCode();
+
+ 
+    casFeat_End = jcas.getRequiredFeatureDE(casType, "End", "uima.cas.Integer", featOkTst);
+    casFeatCode_End  = (null == casFeat_End) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_End).getCode();
+
+ 
+    casFeat_Text = jcas.getRequiredFeatureDE(casType, "Text", "uima.cas.String", featOkTst);
+    casFeatCode_Text  = (null == casFeat_Text) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_Text).getCode();
+
+  }
+}
+
+
+
+    
